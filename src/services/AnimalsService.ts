ecda809ede50f7bead5211279f3a2416628ea79a
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2026-05-21 10:56:16 +0300

    cleanup sources

--- src/services/AnimalsService.ts
+++
@@ -1,9 +0,0 @@
-import { injectable } from "tsyringe";
-import type { IContentService } from "./IContentService";
-
-@injectable()
-export class AnimalsService implements IContentService {
-  getList() { 
-    return ["Кіт", "Пес", "Лисиця", "Ведмідь", "Заєць"]; 
-  }
-}
--- src/services/FruitsService.ts
+++
@@ -1,9 +0,0 @@
-import { injectable } from "tsyringe";
-import type { IContentService } from "./IContentService";
-
-@injectable()
-export class FruitsService implements IContentService {
-  getList() { 
-    return ["Яблуко", "Банан", "Манго", "Ківі", "Апельсин"]; 
-  }
-}
