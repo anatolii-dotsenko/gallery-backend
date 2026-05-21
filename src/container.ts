1edd5a1aeb17d2d21868b83c723459e8fa5314a6
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2026-05-21 11:03:33 +0300

    removing fruits/animals imports

--- src/container.ts
+++ src/container.ts
@@ -1,11 +1,7 @@
 import "reflect-metadata";
 import { container } from "tsyringe";
 import { FileService } from "./services/FileService";
-import { FruitsService } from "./services/FruitsService";
-import { AnimalsService } from "./services/AnimalsService";
 
 container.register("IFileService", { useClass: FileService });
-container.register("fruits", { useClass: FruitsService });
-container.register("animals", { useClass: AnimalsService });
 
 export { container };
--- src/controllers/ImagesController.ts
+++ src/controllers/ImagesController.ts
@@ -5,18 +5,10 @@ import type { IContentService } from "../services/IContentService";
 
 @Controller("/api")
 export class ImagesController {
-  
-  @Get("/list/fruits")
+  @Get("/list/fruits") // todo PhotoService/GalleryService
   getFruits(_req: Request, res: Response) {
     // Отримуємо сервіс через існуючий DI-контейнер
     const service = container.resolve<IContentService>("fruits");
     res.json({ items: service.getList(), type: "fruits" });
   }
-
-  @Get("/list/animals")
-  getAnimals(_req: Request, res: Response) {
-    const service = container.resolve<IContentService>("animals");
-    res.json({ items: service.getList(), type: "animals" });
-  }
-  
 }
